d6157871cad9f787b5895b6a4d75383d01e3a1bc
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-01-04 10:48:24 +0600

    first commit

---
+++ php_01/index.php
@@ -0,0 +1,18 @@
+<?php
+echo "hello digital world";
+
+echo "\n";
+echo "<br>";
+
+class parson{
+    public function detailes($name,$age){
+ echo "Name: ".$name . ", Age:" . $age . "\n";
+    }
+
+    
+}
+$parson1 =new parson();
+    $parson1->detailes("prarona",26);
+
+
+?>
