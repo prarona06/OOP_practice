d1853e372383457ee7be1d925adecc1ba10d50fd
Prarona Dutta <prarona.dutta.06@gmail.com>
2026-01-09 21:07:23 +0600

    add dependency injection

--- php_12/index.php
+++ php_12/index.php
@@ -1,5 +1,27 @@
 <?php
 //dependency injection
 
+class EmailService
+{
+public function  sendEmail ($message)
+{
+    echo "Email sent with message: $message\n";
+}
+}
 
-?>
+
+class UserController
+{
+    protected $email;
+    public function __construct(EmailService $email)
+    {
+        $this->email = $email;
+    }
+    public function notifyUser($message)
+    {
+        $this->email->sendEmail($message);
+    }
+}
+$emailService = new EmailService();
+$userController = new UserController($emailService);
+$userController->notifyUser("Welcome to our platform!");
